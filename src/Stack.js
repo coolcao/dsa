30aa39c2f0250a348b50734fa980ae2876a22df3
coolcao <me@coolcao.com>
2016-11-30 21:51:00 +0800

    update Stack

--- src/Stack.js
+++ src/Stack.js
@@ -1,49 +1,69 @@
 'use strict';
 
 class Stack {
-    constructor(){
+    constructor() {
         this.data = [];
     };
+
     /**
-    * 返回栈的长度
-    **/
-    length(){
+     * 向栈中推入元素
+     * @param  {element} item 要推入的元素
+     * @return {null}      
+     */
+    push(item) {
+        let size = this.size();
+        this.data[size] = item;
+    };
+
+    /**
+     * 删除并返回栈顶元素
+     * @return {element} 栈顶元素
+     */
+    pop() {
+        let size = this.size();
+        let e = this.peek();
+        delete this.data[size - 1];
+        this.data.length = size - 1;
+        return e;
+    };
+
+    /**
+     * 只返回栈顶元素，不修改栈
+     * @return {element} 栈顶元素
+     */
+    peek() {
+        let size = this.size();
+        let e = this.data[size - 1];
+        return e;
+    }
+
+    /**
+     * 判断栈是否为空
+     * @return {Boolean} 栈为空，返回true,否则返回false
+     */
+    isEmpty() {
+        return this.size() === 0;
+    }
+
+    /**
+     * 清空栈
+     * @return {null} 
+     */
+    clear() {
+        this.data = [];
+    };
+
+    /**
+     * 返回栈中元素的个数
+     * @return {Number} 栈中元素的个数
+     */
+    size() {
         return this.data.length;
     };
 
-    /**
-    * 向栈中插入元素
-    **/
-    push(item){
-        this.data.push(item);
-    };
-
-    /**
-    * 删除并返回栈顶元素
-    **/
-    pop(){
-        return this.data.pop();
-    };
-
-    /**
-    * 清空栈
-    **/
-    clear(){
-        this.data = [];
-    };
-
-    /**
-    * 只返回栈顶元素，不删除
-    **/
-    peek(){
-        return this.data.slice(this.length() - 1)[0];
-    }
-    toString(){
+    toString() {
         return this.data;
     };
 };
 
-
- module.exports = Stack;
-
-
+module.exports = Stack;
