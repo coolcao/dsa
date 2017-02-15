57f5654401d8c40f003381f311eda7d043be6ecc
coolcao <me@coolcao.com>
2017-02-15 23:59:05 +0800

    二分查找递归

--- src/Search/binSearch.js
+++ src/Search/binSearch.js
@@ -27,16 +27,43 @@ let binSearch = function(arr, data) {
 }
 
 /**
+ * 递归二分查找
+ * @param  {Array} arr  要查找的数组
+ * @param  {Number} data 要查找的数据
+ * @return {Number}      索引位置
+ */
+let binSearchByRecursion = function(arr, data) {
+    let search = function(start, end) {
+        if (start <= end) {
+            let mid = ((start + end) / 2) >>> 0;
+            if (arr[mid] === data) {
+                return mid;
+            } else {
+                if (arr[mid] < data) {
+                    start = mid + 1;
+                } else if (arr[mid] > data) {
+                    end = mid - 1;
+                }
+                return search(start, end);
+            }
+        }else{
+            return -1;
+        }
+    }
+    return search(0, arr.length-1);
+}
+
+/**
  * 计算重复次数
  * @param  {Array} arr  要统计的数组
  * @param  {Number} data 要统计的元素
  * @return {Number}      统计个数
  */
-let count = function (arr,data) {
-    let pos = binSearch(arr,data);
+let count = function(arr, data) {
+    let pos = binSearch(arr, data);
     let count = 0;
-    if(pos > -1){
-        count ++;
+    if (pos > -1) {
+        count++;
 
         /**
          * 二分查找数组为已排序数组
@@ -49,11 +76,11 @@ let count = function (arr,data) {
         let x = pos - 1;
         let y = pos + 1;
         while (arr[x] === data || arr[y] === data) {
-            if(arr[x] === data){
-                count ++;
+            if (arr[x] === data) {
+                count++;
             }
-            if(arr[y] === data){
-                count ++;
+            if (arr[y] === data) {
+                count++;
             }
             x--;
             y++;
@@ -68,7 +95,10 @@ let count = function (arr,data) {
 //     return item % 2 == 0;
 // });
 
-let arr = [1,2,3,3,3,3,4,4,5];
+let arr = [1, 2, 3, 3, 3, 3, 4, 4, 5, 6,6,7,7,8,8,9,10,11,12,14];
 
-console.log(count(arr,9));
+console.log(binSearchByRecursion(arr, 8));
+// console.log('==========');
+// console.log(binSearch(arr, 8));
+
 module.exports = binSearch;
